7c81a86e95fb03458b9c98d41c91d699427a7bf0
Ivan <122042934+IvanGrebenyukov@users.noreply.github.com>
2023-05-23 15:50:37 +0500

    Add files via upload

---
+++ Practice16_ind2_grebenukov/Zadanie3_forms/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Practice15_Grebenukov
+{
+    public partial class Form1 : Form
+    {
+        ArrayList numbers = new ArrayList();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+                
+
+
+        }
+
+        private void buttonLoadFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openFileDialog1 = new OpenFileDialog();
+                openFileDialog1.Filter = "Text Files|*.txt";
+                openFileDialog1.ToString();
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    string[] lines = File.ReadAllLines(openFileDialog1.FileName);
+                    if(lines.Length == 0)
+                    {
+                        MessageBox.Show("Файл пуст", "Ошибка");
+                    }
+                    else
+                    {
+                        foreach (string line in lines)
+                        {
+                            string[] parts = line.Split(';');
+                            double real = double.Parse(parts[0]);
+                            double imaginary = double.Parse(parts[1].Replace("i", ""));
+                            ComplexNumber number = new ComplexNumber(real, imaginary);
+                            numbers.Add(number);
+                        }
+
+                        foreach (ComplexNumber number in numbers)
+                        {
+                            listBoxComplexNumber.Items.Add($"{number.Real};{number.Imaginary}");
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ошибка");
+            }
+        }
+
+        private void OperationsButtonFile_Click(object sender, EventArgs e)
+        {
+            var count = listBoxComplexNumber.SelectedItems.Count;
+
+            if(count == 2)
+            {
+                string result = "";
+                string selectedItems = String.Join(";", listBoxComplexNumber.SelectedItems.Cast<string>().ToArray());
+                string[] parts = selectedItems.Split(";");
+                double real1 = double.Parse(parts[0]);
+                double imaginary1 = double.Parse(parts[1]);
+                double real2 = double.Parse(parts[2]);
+                double imaginary2 = double.Parse(parts[3]);
+                ComplexNumber number1 = new ComplexNumber(real1, imaginary1);
+                ComplexNumber number2 = new ComplexNumber(real2, imaginary2);
+                if (comboBoxOperations.SelectedItem == null)
+                {
+                    MessageBox.Show("Выберите операцию в ComboBox", "Ошибка");
+                }
+                else
+                {
+                    string operation = comboBoxOperations.SelectedItem.ToString();
+                    switch (operation)
+                    {
+                        case "Сложение":
+                            result = number1.Add(number2).ToString();
+                            MessageBox.Show(result, "Ответ");
+                            break;
+                        case "Умножение":
+                            result = number1.Multiply(number2).ToString();
+                            MessageBox.Show(result, "Ответ");
+                            break;
+                        case "Вычитание":
+                            result = number1.Subtract(number2).ToString();
+                            MessageBox.Show(result, "Ответ");
+                            break;
+                    }
+                }
+                
+            }
+            else
+            {
+                MessageBox.Show("Выберите два значения", "Ошибка");
+            }
+        }
+
+        private void OperationsButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string result = "";
+                double real1 = double.Parse(textBoxNumber1.Text);
+                double imaginary1 = double.Parse(textBoxNumber2.Text);
+                double real2 = double.Parse(textBoxNumber3.Text);
+                double imaginary2 = double.Parse(textBoxNumber4.Text);
+                ComplexNumber number1 = new ComplexNumber(real1, imaginary1);
+                ComplexNumber number2 = new ComplexNumber(real2, imaginary2);
+                if (comboBoxOperations.SelectedItem == null)
+                {
+                    MessageBox.Show("Выберите операцию в ComboBox", "Ошибка");
+                }
+                else
+                {
+                    string operation = comboBoxOperations.SelectedItem.ToString();
+                    switch (operation)
+                    {
+                        case "Сложение":
+                            result = number1.Add(number2).ToString();
+                            MessageBox.Show(result, "Ответ");
+                            break;
+                        case "Умножение":
+                            result = number1.Multiply(number2).ToString();
+                            MessageBox.Show(result, "Ответ");
+                            break;
+                        case "Вычитание":
+                            result = number1.Subtract(number2).ToString();
+                            MessageBox.Show(result, "Ответ");
+                            break;
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Введены неверные данные");
+            }
+        }
+    }
+}
---
+++ Practice16_ind2_grebenukov/Zadanie4/Program.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+namespace Zadanie4
+{
+	class Program
+	{
+		static Hashtable catalog = new Hashtable();
+
+		static void Main(string[] args)
+		{
+			bool check = false;
+
+			while (!check)
+			{
+
+				Console.WriteLine("Выберите действие:\n" +
+								  "1. Добавить новый диск\n" +
+								  "2. Удалить диск\n" +
+								  "3. Добавить песню на диск\n" +
+								  "4. Удалить песню с диска\n" +
+								  "5. Просмотреть каталог\n" +
+								  "6. Просмотреть содержимое диска\n" +
+								  "7. Поиск всех записей определенного исполнителя\n" +
+								  "8. Выход");
+
+				string choise = Console.ReadLine();
+
+				switch (choise)
+				{
+					case "1": AddDisk(); break;
+					case "2": DeleteDisk(); break;
+					case "3": AddSong(); break;
+					case "4": DeleteSong(); break;
+					case "5": DisplayCatalog(); break;
+					case "6": DisplayDisk(); break;
+					case "7": SearchByArtist(); break;
+					case "8": check = true; break;
+					default:
+					{
+						Console.WriteLine("Ошибка! Повторите ввод!");
+					}
+					break;
+				}
+			}
+		}
+		private static void AddDisk()
+		{
+			Console.Clear();
+			Console.Write("Введите название диска: ");
+			string nameDisk = Console.ReadLine();
+
+			if (catalog.ContainsKey(nameDisk))
+			{
+				Console.WriteLine("Такой диск уже существует");
+			}
+			else
+			{
+				catalog[nameDisk] = new ArrayList();
+				Console.WriteLine("Диск был добавлен");
+			}
+		}
+		private static void DeleteDisk()
+		{
+			Console.Clear();
+			if (catalog.Count == 0)
+			{
+				Console.WriteLine("Данный каталог пуст");
+			}
+			else
+			{
+				Console.Write("Введите название диска: ");
+				string nameDisk = Console.ReadLine();
+
+				if (catalog.ContainsKey(nameDisk))
+				{
+					catalog.Remove(nameDisk);
+					Console.WriteLine("Диск был удален");
+				}
+				else
+				{
+					Console.WriteLine("Такого диска не существует");
+				}
+			}
+		}
+		private static void AddSong()
+		{
+			Console.Clear();
+			Console.Write("Введите название диска: ");
+			string nameDisk = Console.ReadLine();
+
+			if (catalog.ContainsKey(nameDisk))
+			{
+				ArrayList songs = (ArrayList)catalog[nameDisk];
+
+				Console.Write("Введите название песни: ");
+				string nameSong = Console.ReadLine();
+				Console.Write("Введите имя исполнителя: ");
+				string nameArtist = Console.ReadLine();
+
+				if (songs.Contains(nameSong))
+				{
+					Console.WriteLine("Такая песня уже есть на этом диске.");
+				}
+				else
+				{
+					songs.Add(string.Concat(nameSong, " ", nameArtist));
+					Console.WriteLine("Песня была добавлена");
+				}
+			}
+			else
+			{
+				Console.WriteLine("Такого диска не существует");
+			}
+		}
+		private static void DeleteSong()
+		{
+			Console.Clear();
+			if (catalog.Count == 0)
+			{
+				Console.WriteLine("Данный каталог пуст");
+			}
+			else
+			{
+				Console.Write("Введите название диска: ");
+				string nameDisk = Console.ReadLine();
+
+				if (catalog.Contains(nameDisk))
+				{
+					ArrayList songs = (ArrayList)catalog[nameDisk];
+
+					if (songs.Count == 0)
+					{
+						Console.WriteLine($"Диск {nameDisk} пуст");
+					}
+					else
+					{
+						Console.Write("Введите название песни: ");
+						string nameSong = Console.ReadLine();
+
+						if (songs.Contains(nameSong))
+						{
+							songs.Remove(nameSong);
+							Console.WriteLine("Песня была удалена");
+						}
+						else
+						{
+							Console.WriteLine($"Такая песня не существует на диске {nameDisk}.");
+						}
+					}
+				}
+				else
+				{
+					Console.WriteLine("Такого диска не существует");
+				}
+			}
+		}
+		private static void DisplayDisk()
+		{
+			Console.Clear();
+			if (catalog.Count == 0)
+			{
+				Console.WriteLine("Данный каталог пуст");
+			}
+			else
+			{
+				Console.Write("Введите название диска: ");
+				string nameDisk = Console.ReadLine();
+
+				if (catalog.ContainsKey(nameDisk))
+				{
+					ArrayList songs = (ArrayList)catalog[nameDisk];
+
+					if (songs.Count == 0)
+					{
+						Console.WriteLine($"Диск {nameDisk} пуст");
+					}
+					else
+					{
+						Console.WriteLine($"Содержимое диска {nameDisk}: ");
+
+						int i = 1;
+						foreach (string nameSong in songs) 
+						{ 
+							Console.WriteLine($"{i}: {nameSong}"); i++; 
+						}
+					}
+				}
+				else
+				{
+					Console.WriteLine("Такого диска не существует");
+				}
+			}
+		}
+		private static void DisplayCatalog()
+		{
+			Console.Clear();
+
+			if (catalog.Keys.Count == 0)
+			{
+				Console.WriteLine("Данный каталог пуст");
+			}
+			else
+			{
+				Console.WriteLine("Содержимое каталога:");
+
+				int i = 1;
+				foreach (string nameDisk in catalog.Keys) 
+				{ 
+					Console.WriteLine($"{i}: {nameDisk}"); i++; 
+				}
+			}
+		}
+
+		
+
+		
+		private static void SearchByArtist()
+		{
+			Console.Clear();
+			if (catalog.Count == 0)
+			{
+				Console.WriteLine("Данный каталог пуст");
+			}
+			else
+			{
+				Console.Write("Введите имя исполнителя: ");
+				string nameArtist = Console.ReadLine();
+				bool found = false;
+				int i = 0;
+
+				foreach (ArrayList songs in catalog.Values)
+				{
+
+					foreach (string nameSong in songs)
+					{
+						if (nameSong.Contains(nameArtist))
+						{
+							Console.WriteLine($"{nameSong}");
+							found = true; i++;
+						}
+					}
+				}
+
+				if (!found)
+				{
+					Console.WriteLine("Записи исполнителя не найдены");
+				}
+				else
+				{
+					Console.WriteLine($"Найдены записи в количестве: {i}");
+				}
+			}
+		}
+
+
+
+
+	}
+
+}
+
+
